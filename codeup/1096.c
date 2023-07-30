c0cfa5772102330cac1f94c97697cb86a6dc7728
redzzzi <myqueen1346@naver.com>
2023-07-30 23:12:06 +0900

    codeup

--- codeup/1096.c
+++ codeup/1096.c
@@ -3,5 +3,18 @@
 #include<stdio.h>
 
 int main(void) {
-
+  int n, i, j, x, y;
+  int board[20][20]={};
+  scanf("%d", &n);
+  for (i=1; i<=n; i++) {
+    scanf("%d %d", &x, &y);
+    board[x][y]=1;
+  }
+  for (i=1; i<=19; i++) {
+    for (j=1; j<=19; j++) {
+      printf("%d ", board[i][j]);
+    }
+    printf("\n");
+  }
+  return 0;
 }
