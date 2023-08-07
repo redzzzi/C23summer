1ef460a07575e3df701f3343c8c20e21ec53cc40
redzzzi <myqueen1346@naver.com>
2023-08-07 14:22:48 +0900

    week4

--- week4/test.c
+++ week4/test.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
 
+int add (int a, int b);
+
 int main(void) { 
 // int: return type, main: func name, void: parameter type
 
+  int result = add(1, 2);
   printf("Hello, world!\n");
+  printf("The result is %d.\n", result);
   return 0;
+}
+
+int add(int a, int b) {
+  return a+b;
 }
