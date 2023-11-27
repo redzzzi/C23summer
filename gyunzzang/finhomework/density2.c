25ae8c62dc5cc492126a8ab7a5f3be7afd78ff07
redzzzi <myqueen1346@naver.com>
2023-11-27 09:35:52 +0900

    density

--- gyunzzang/finhomework/density2.c
+++ gyunzzang/finhomework/density2.c
@@ -1,39 +1,38 @@
-#include <stdio.h>
+// 2023-11-24
+// 10.2 Programming Assignment 10: density.c
+#include<stdio.h>
 
-// 전체 공식
+// whole formula
 double density(int p, int A) {
-  return (double)p / A * 100;
+  return (double)p/A*100;
 }
 
 int main() {
   int row = 0, col = 0, alpha = 0;
   char type;
-
-  // 행의 개수를 입력받음
-  scanf("%d", &col);
-
-  // 개행 문자를 처리
-  scanf("%c", &type);
-
-  // 입력 받은 행의 개수만큼 반복
-  for (int i = 1; i <= col; i++) {
-    for (int j = 1; j <= row; j++) {
-      // 문자를 입력 받음
-      scanf("%c", &type);
-
-      // 알파벳인 경우에만 alpha를 증가시키고, 띄어쓰기인 경우에는 행을 증가
-      if (('a' <= type && type <= 'z') || ('A' <= type && type <= 'Z')) {
-        alpha++;
+  int cmp = 0;
+  scanf("%d\n", &col);
+  for (int i=1; i<=col;) {
+    scanf("%c", &type);
+    if (type == '\n') {
+      if (cmp < row) {
+        cmp = row;
       }
-
-      // 다음 행으로 넘어가기 전에 개행 문자를 읽음
-      scanf("%c", &type);
+      i++;
+      row = 0;
+    }
+    if (type == ' ') {
+      row++;  
+    }
+    if (('a' <= type && type <= 'z') || ('A' <= type && type <= 'Z')) {
+      row++;
+      alpha++;
     }
   }
-
   int area = row * col;
+  printf("%f\n", density(alpha, area));
   printf("%.2f\n", density(alpha, area));
+  // printf("row : %d\ncol : %d\n", row, col);
 
-  return 0;
+  return 0;  
 }
-
